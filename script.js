3b5f3c340ef63cff868ce72c48a695e9c03d8390
Gabyyyyiy <gabrieladasilvanunex@gmail.com>
2025-07-06 19:55:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarSurpresa() {
+  const resposta = document.getElementById('resposta');
+  resposta.innerHTML = "Malu, você é aquela amiga que eu quero pra vida toda. Agradeço a Deus por ter você comigo. 💙✨";
+}
