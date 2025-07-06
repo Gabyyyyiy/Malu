6408dce5c95fdc083b63aab5fabb1f8579be34c4
Gabyyyyiy <gabrieladasilvanunex@gmail.com>
2025-07-06 20:09:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function mostrarSurpresa() {
   const resposta = document.getElementById('resposta');
-  resposta.innerHTML = "Malu, você é aquela amiga que eu quero pra vida toda. Agradeço a Deus por ter você comigo. 💙✨";
+  resposta.innerHTML = "Malu, você é a pessoa que eu quero pra vida toda. Agradeço a Deus por ter você comigo. 💙✨";
 }
